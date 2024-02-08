beac23ade8872e8d2730ef9a086728263e70a2ca
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-02-08 13:43:58 +0530

    Update MonetaryCurrency.java

--- fineract-core/src/main/java/org/apache/fineract/organisation/monetary/domain/MonetaryCurrency.java
+++ fineract-core/src/main/java/org/apache/fineract/organisation/monetary/domain/MonetaryCurrency.java
@@ -48,7 +48,7 @@ public class MonetaryCurrency {
 
     public MonetaryCurrency copy() {
         return new MonetaryCurrency(this.code, this.digitsAfterDecimal, this.inMultiplesOf);
-        // efderfref
+        // efderfrefede
     }
 
     public static MonetaryCurrency fromApplicationCurrency(ApplicationCurrency applicationCurrency) {
